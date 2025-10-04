c7c6f8b744c4ca0c9a9f451ecb36f5351930a95c
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-04 11:21:58 -0300

    Logica de dominio de Dentist

--- backend/src/Domain/Entities/Dentist.cs
+++ backend/src/Domain/Entities/Dentist.cs
@@ -7,9 +7,15 @@ public class Dentist : User
 
     public Dentist() : base() { }
 
-    public Dentist(string firstName, string lastName, string email, string password, string licenseNumber) : base ( firstName, lastName, email, password)
+    public Dentist(string firstName, string lastName, string email, string password, string licenseNumber) : base(firstName, lastName, email, password)
     {
+        ValidateLicenseNumber(licenseNumber);
         LicenseNumber = licenseNumber;
     }
+
+    private static void ValidateLicenseNumber(string licenseNumber)
+    {
+        if (string.IsNullOrWhiteSpace(licenseNumber))
+            throw new ArgumentException("La matrícula es obligatoria.");
+    }
     
-}
