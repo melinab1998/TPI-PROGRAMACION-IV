364fa448e417d3b30f82a7d624955c2ddafa34b7
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-04 11:46:21 -0300

    Logica de dominio de User  (agrego private set)

--- backend/src/Domain/Entities/User.cs
+++ backend/src/Domain/Entities/User.cs
@@ -4,13 +4,13 @@ using System.Text.RegularExpressions;
 public abstract class User
 {
 
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public int Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
 
-    public string Email { get; set; }
+    public string Email { get; private set; }
 
-    protected string Password { get; set; }
+    protected string Password { get; private set; }
 
 
 
