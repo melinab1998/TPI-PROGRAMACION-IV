3f723f56524f49efbaedb03ad255eff878879ca3
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-04 09:47:05 -0300

    Logica de dominio User

--- backend/src/Domain/Entities/User.cs
+++ backend/src/Domain/Entities/User.cs
@@ -1,4 +1,5 @@
 namespace Domain.Entities;
+using System.Text.RegularExpressions;
 
 public abstract class User
 {
@@ -16,12 +17,44 @@ public abstract class User
     protected User() { }
 
     protected User(string firstName, string lastName, string email, string password)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            Password = password;
-        }
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+
+        ValidateUserData();
+    }
+
+
+    private void ValidateUserData()
+    {
+        if (string.IsNullOrWhiteSpace(FirstName))
+            throw new ArgumentException("El nombre no puede estar vacío.");
+
+        if (string.IsNullOrWhiteSpace(LastName))
+            throw new ArgumentException("El nombre no puede estar vacío.");
+
+        if (string.IsNullOrWhiteSpace(Email))
+            throw new ArgumentException("El email no puede estar vacío.");
+
+        if (!Regex.IsMatch(Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            throw new ArgumentException("El formato del email no es válido.");
+
+        if (string.IsNullOrWhiteSpace(Password))
+            throw new ArgumentException("La contraseña no puede estar vacío.");
+
+         if (Password.Length < 6)
+            throw new ArgumentException("La contraseña debe tener al menos 6 caracteres.");
+
+        if (!Regex.IsMatch(Password, @"^(?=.*[A-Z])(?=.*\d).+$"))
+            throw new ArgumentException("La contraseña debe contener al menos una mayúscula y un número.");
+    }
+    
+    public bool VerifyPassword(string password)
+    {
+        return Password == password;
+    }
 
 
 }
--- backend/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ backend/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e90eb30f7f1bb3b9fb93ed89cd0c08d1581ed2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41123fdb0a02d5945cb46f8da2194c08e5b4a323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
