680af224ce50c6ebe894ae146768e99e388c1db6
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-04 17:41:22 -0300

    Logica de dominio de HealthPlan

--- backend/src/Domain/Entities/HealthPlan.cs
+++ backend/src/Domain/Entities/HealthPlan.cs
@@ -8,14 +8,27 @@ public class HealthPlan
 
     //Foreign Key    
     public int HealthInsuranceId { get; set; }
-    
+
     // Referencia a la obra social que pertenece un determinado plan
     public HealthInsurance HealthInsurance { get; set; } = null!;
 
     public HealthPlan() { }
 
-    public HealthPlan( string name)
+    public HealthPlan(string name)
     {
+        ValidateName(name);
         Name = name;
+    }
+
+    public static void ValidateName(string name)
+     {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("El nombre del plan no puede estar vacío.");   
+    }
+
+    public void SetHealthInsurance(HealthInsurance healthInsurance)
+    {
+        HealthInsurance = healthInsurance ?? throw new ArgumentException("La obra social no puede ser nula.");
+        HealthInsuranceId = healthInsurance.Id;
     }
 }
