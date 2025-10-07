8deecc06d007fc02825cf102ba68f3e479a77959
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-07 17:54:06 -0300

    Arreglos

--- backend/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ backend/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6f83c2d6190c0bba39e52dac836c878cc8305b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975125c5c148ce3763656b441b406c060962f810")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
