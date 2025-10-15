aba72c4cf4e92a3ff7db978679e46897847dcb87
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-14 21:06:25 -0300

    Data Annotations minlength  firstname

--- backend/src/Web/Models/Requests/CreateDentistRequest.cs
+++ backend/src/Web/Models/Requests/CreateDentistRequest.cs
@@ -5,6 +5,7 @@ namespace Web.Models.Requests;
 public record CreateDentistRequest
 (
      [Required(ErrorMessage = "El nombre es obligatorio")]
+     [MinLength(2, ErrorMessage = "Debe tener al menos 2 caracteres")]
      string FirstName,
      [Required] string LastName,
      [Required] string Email,
