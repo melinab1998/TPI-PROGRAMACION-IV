8fb27083fa3e54780cb8af5a25f91705f4958e26
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-20 21:36:58 -0300

    Modificacion HealthPlan

--- backend/src/Domain/Interfaces/IHealthPlanRepository.cs
+++ backend/src/Domain/Interfaces/IHealthPlanRepository.cs
@@ -6,5 +6,4 @@ public interface IHealthPlanRepository
 {
     IEnumerable<HealthPlan> GetAll();
     IEnumerable<HealthPlan> GetByInsuranceId(int healthInsuranceId);
-    HealthPlan? GetById(int id);
 }
