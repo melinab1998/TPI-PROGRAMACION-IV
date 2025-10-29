95824d54f33dcb64c8f90ed8bdea5710c963a6ba
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-10-29 17:34:12 -0300

    add UpdatePersonalInfo and ChangePassword methods in Patient

--- backend/src/Domain/Entities/Patient.cs
+++ backend/src/Domain/Entities/Patient.cs
@@ -18,4 +18,32 @@ public class Patient : User
     {
         Dni = dni;
     }
+
+    public void ChangePassword(string newHashedPassword)
+    {
+        SetPassword(newHashedPassword);
+    }
+
+    public void UpdatePersonalInfo(
+        string? firstName,
+        string? lastName,
+        string? email,
+        string? address,
+        string? phoneNumber,
+        string? city,
+        string? membershipNumber,
+        DateOnly? birthDate,
+        int? healthPlanId
+    )
+    {
+        if (!string.IsNullOrEmpty(firstName)) FirstName = firstName;
+        if (!string.IsNullOrEmpty(lastName)) LastName = lastName;
+        if (!string.IsNullOrEmpty(email)) Email = email;
+        if (!string.IsNullOrEmpty(address)) Address = address;
+        if (!string.IsNullOrEmpty(phoneNumber)) PhoneNumber = phoneNumber;
+        if (!string.IsNullOrEmpty(city)) City = city;
+        if (!string.IsNullOrEmpty(membershipNumber)) MembershipNumber = membershipNumber;
+        if (birthDate.HasValue) BirthDate = birthDate;
+        if (healthPlanId.HasValue) HealthPlanId = healthPlanId.Value;
+    }
 }
