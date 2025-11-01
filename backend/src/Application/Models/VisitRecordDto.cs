25bf4f4aee07aa8920f1a2890e81c59d58028465
Ivan Berrino <ivanberrino10@hotmail.com>
2025-11-01 19:34:26 -0300

    Creacion dto de respuesta

---
+++ backend/src/Application/Models/VisitRecordDto.cs
@@ -0,0 +1,18 @@
+using Domain.Entities;
+
+namespace Application.Models;
+
+public record VisitRecordDto(int Id, DateOnly VisitDate, string Treatment, string Diagnosis, string? Notes, string? Prescription, int TurnId)
+{
+    public static VisitRecordDto Create(VisitRecord entity)
+    {
+        var dto = new VisitRecordDto(entity.Id, entity.VisitDate, entity.Treatment, entity.Diagnosis, entity.Notes, entity.Prescription, entity.TurnId);
+
+        return dto;
+    }
+    public static List<VisitRecordDto> Create(IEnumerable<VisitRecord> visitRecords)
+    {
+        return visitRecords.Select(visitRecord => Create(visitRecord)).ToList();
+    }
+
+}
