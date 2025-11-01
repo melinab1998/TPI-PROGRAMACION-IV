a0fecfc98985568476f98d9733d780ecd9ac9564
Ivan Berrino <ivanberrino10@hotmail.com>
2025-11-01 19:36:53 -0300

    Orquestacion de tareas del visitRecord

---
+++ backend/src/Application/Services/VisitRecordService.cs
@@ -0,0 +1,74 @@
+using Application.Interfaces;
+using Application.Models;
+using Application.Models.Requests;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Services
+{
+    public class VisitRecordService : IVisitRecordService
+    {
+        private readonly IVisitRecordRepository _visitRecordRepository;
+        private readonly ITurnRepository _turnRepository;
+        public VisitRecordService(
+            IVisitRecordRepository visitRecordRepository,
+            ITurnRepository turnRepository)
+        {
+            _visitRecordRepository = visitRecordRepository;
+            _turnRepository = turnRepository;
+        }
+
+        public List<VisitRecordDto> GetAllVisitRecord()
+        {
+            var visitRecords = _visitRecordRepository.List();
+            if (!visitRecords.Any())
+                return new List<VisitRecordDto>();
+            return VisitRecordDto.Create(visitRecords);
+        }
+
+        public VisitRecordDto GetVisitRecordById(int id)
+        {
+            var visitRecord = _visitRecordRepository.GetById(id)
+                ?? throw new NotFoundException("VISIT_RECORD_NOT_FOUND");
+            return VisitRecordDto.Create(visitRecord);
+        }
+
+        public VisitRecordDto CreateVisitRecord(CreateVisitRecordRequest request)
+        {
+            var turn = _turnRepository.GetById(request.TurnId)
+                ?? throw new NotFoundException("TURN_NOT_FOUND");
+            var visitRecord = new VisitRecord(
+                request.VisitDate,
+                request.Treatment,
+                request.Diagnosis,
+                request.Notes,
+                request.Prescription,
+                request.TurnId
+            );
+            _visitRecordRepository.Add(visitRecord);
+            return VisitRecordDto.Create(visitRecord);
+        }
+
+        public VisitRecordDto UpdateVisitRecord(int id, UpdateVisitRecordRequest request)
+        {
+            var visitRecord = _visitRecordRepository.GetById(id)
+                ?? throw new NotFoundException("VISIT_REPOSITORY_NOT_FOUND");
+            visitRecord.UpdateInfo(
+                request.VisitDate,
+                request.Treatment,
+                request.Diagnosis,
+                request.Notes,
+                request.Prescription,
+                request.TurnId
+            );
+            _visitRecordRepository.Update(visitRecord);
+
+            return VisitRecordDto.Create(visitRecord);
+        }
+    }
+}
