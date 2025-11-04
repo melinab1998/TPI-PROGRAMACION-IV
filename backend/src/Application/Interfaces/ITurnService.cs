ffa1e36ab90541720150d5bf43fdf4e42ddb7b85
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-11-04 20:11:38 -0300

    Cambios en la interfaz

--- backend/src/Application/Interfaces/ITurnService.cs
+++ backend/src/Application/Interfaces/ITurnService.cs
@@ -5,10 +5,13 @@ namespace Application.Interfaces
 {
     public interface ITurnService
     {
-        IEnumerable<TurnDto> GetAllTurns();
+        List<TurnDto> GetAllTurns();
         TurnDto GetTurnById(int id);
         TurnDto CreateTurn(CreateTurnRequest request);
         TurnDto UpdateTurn(int id, UpdateTurnRequest request);
+        void DeleteTurn(int id);
         void CancelTurn(int id);
+
+        
     }
 }
