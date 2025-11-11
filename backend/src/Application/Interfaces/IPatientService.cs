a8105e791adbd4a584a993cf12ce85abc8d03147
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-11-11 18:30:42 -0300

    Agregando nuevo pararametros para validaciones de autenticacion

--- backend/src/Application/Interfaces/IPatientService.cs
+++ backend/src/Application/Interfaces/IPatientService.cs
@@ -6,12 +6,12 @@ namespace Application.Interfaces;
 public interface IPatientService
 {
     List<PatientDto> GetAllPatients();
-    PatientDto GetPatientById(int id);
+    PatientDto GetPatientById(int id, string userId, string userRole);
     PatientDto RegisterPatient(RegisterPatientRequest request);
     PatientDto UpdatePatient(int id, UpdatePatientRequest request);
     ActivationResponseDto<PatientDto> CreatePatientByDentist(CreatePatientByDentistRequest request);
     void ActivatePatient(string token, string password);
-    PatientDto UpdatePatientEmail(int id, UpdatePatientEmailRequest request);
-    void UpdatePatientPassword(int id, UpdatePatientPasswordRequest request);
+    PatientDto UpdatePatientEmail(int id, UpdatePatientEmailRequest request, string userId, string userRole);
+    void UpdatePatientPassword(int id, UpdatePatientPasswordRequest request, string userId, string userRole);
 }
 
